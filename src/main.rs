f22420f2b40298e7926c0bc7ddc7a0cc065e3d85
LeoL6 <87842339+LeoL6@users.noreply.github.com>
2025-02-17 19:03:16 -0400

    Basic application setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::*;
+use egui::{CentralPanel, IconData};
+
+#[derive(Default)]
+struct TriangleGator {}
+
+fn main() -> Result {
+    // let icon_data = IconData::from("path/to/icon.png"); // Load an icon from a file
+
+    let options = NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+        .with_inner_size([600.0, 600.0])
+        .with_resizable(false),
+        // .with_icon(icon_data),
+        ..Default::default()
+    };
+
+    run_native(
+        "Triangle Gator", 
+        options, 
+        Box::new(|cc| {
+            Ok(Box::new(TriangleGator::default()))
+        }) 
+    )
+}
+
+impl App for TriangleGator {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello!");
+        });
+    }
 }
