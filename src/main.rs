7cb6c4cf14d3c7b623b3ecbe60bf77ade1a0aef4
LeoL6 <87842339+LeoL6@users.noreply.github.com>
2025-02-17 20:55:23 -0400

    Implemented Custom Frame and locked to dark theme

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use eframe::*;
-use egui::{CentralPanel, IconData};
+use egui::{IconData, Theme, ViewportCommand};
 
 #[derive(Default)]
 struct TriangleGator {}
 
 fn main() -> Result {
-    // let icon_data = IconData::from("path/to/icon.png"); // Load an icon from a file
-
+    let icon_image = image::open("assets/gator-icon.png").expect("Should be able to open icon PNG file");
+    let width = icon_image.width();
+    let height = icon_image.height();
+    let icon_rgba8 = icon_image.into_rgba8().to_vec();
+    let icon_data =IconData{
+            rgba: icon_rgba8,
+            width,
+            height,
+    };
+        
     let options = NativeOptions {
         viewport: egui::ViewportBuilder::default()
-        .with_inner_size([600.0, 600.0])
-        .with_resizable(false),
-        // .with_icon(icon_data),
+        .with_inner_size([330.0, 400.0])
+        .with_resizable(false)
+        .with_decorations(false)
+        .with_transparent(true)
+        .with_icon(icon_data),
         ..Default::default()
     };
 
@@ -25,9 +37,113 @@ fn main() -> Result {
 }
 
 impl App for TriangleGator {
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        egui::Rgba::TRANSPARENT.to_array() // Make sure we don't paint anything behind the rounded corners
+    }
+
     fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
-        CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello!");
+        custom_window_frame(ctx, "Triangle Gator", |ui| {
+            ctx.set_theme(Theme::Dark);
+            ui.label("Hello :P");
         });
+    }
+}
+
+fn custom_window_frame(ctx: &egui::Context, title: &str, add_contents: impl FnOnce(&mut egui::Ui)) {
+    use egui::{CentralPanel, UiBuilder};
+
+    let panel_frame = egui::Frame::new()
+        .fill(ctx.style().visuals.window_fill())
+        .corner_radius(10)
+        .stroke(ctx.style().visuals.widgets.noninteractive.fg_stroke)
+        .outer_margin(1); // so the stroke is within the bounds
+
+    CentralPanel::default().frame(panel_frame).show(ctx, |ui| {
+        let app_rect = ui.max_rect();
+
+        let title_bar_height = 32.0;
+        let title_bar_rect = {
+            let mut rect = app_rect;
+            rect.max.y = rect.min.y + title_bar_height;
+            rect
+        };
+        title_bar_ui(ui, title_bar_rect, title);
+
+        // Add the contents:
+        let content_rect = {
+            let mut rect = app_rect;
+            rect.min.y = title_bar_rect.max.y;
+            rect
+        }
+        .shrink(4.0);
+        let mut content_ui = ui.new_child(UiBuilder::new().max_rect(content_rect));
+        add_contents(&mut content_ui);
+    });
+}
+
+fn title_bar_ui(ui: &mut egui::Ui, title_bar_rect: eframe::epaint::Rect, title: &str) {
+    use egui::{vec2, Align2, FontId, Id, PointerButton, Sense, UiBuilder};
+
+    let painter = ui.painter();
+
+    let title_bar_response = ui.interact(
+        title_bar_rect,
+        Id::new("title_bar"),
+        Sense::click_and_drag(),
+    );
+
+    // Paint the title:
+    painter.text(
+        title_bar_rect.center(),
+        Align2::CENTER_CENTER,
+        title,
+        FontId::proportional(20.0),
+        ui.style().visuals.text_color(),
+    );
+
+    // Paint the line under the title:
+    painter.line_segment(
+        [
+            title_bar_rect.left_bottom() + vec2(1.0, 0.0),
+            title_bar_rect.right_bottom() + vec2(-1.0, 0.0),
+        ],
+        ui.visuals().widgets.noninteractive.bg_stroke,
+    );
+
+    // Interact with the title bar (drag to move window):
+    if title_bar_response.double_clicked() {
+        let is_maximized = ui.input(|i| i.viewport().maximized.unwrap_or(false));
+        ui.ctx()
+            .send_viewport_cmd(ViewportCommand::Maximized(!is_maximized));
+    }
+
+    if title_bar_response.drag_started_by(PointerButton::Primary) {
+        ui.ctx().send_viewport_cmd(ViewportCommand::StartDrag);
+    }
+
+    ui.allocate_new_ui(
+        UiBuilder::new()
+            .max_rect(title_bar_rect)
+            .layout(egui::Layout::right_to_left(egui::Align::Center)),
+        |ui| {
+            ui.spacing_mut().item_spacing.x = 0.0;
+            ui.visuals_mut().button_frame = false;
+            ui.add_space(8.0);
+            close_ui(ui);
+        },
+    );
+}
+
+/// Show close button for the native window.
+fn close_ui(ui: &mut egui::Ui) {
+    use egui::{Button, RichText};
+
+    let button_height = 12.0;
+
+    let close_response = ui
+        .add(Button::new(RichText::new("❌").size(button_height)))
+        .on_hover_text("Close the window");
+    if close_response.clicked() {
+        ui.ctx().send_viewport_cmd(ViewportCommand::Close);
     }
 }
